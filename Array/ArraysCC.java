f0f35f00809fbe5cf6015f00bb5b39b2d691bb83
bhanwarlal11 <bhawarpatel2016@gmail.com>
2024-02-04 17:53:55 +0530

    linearSearchpush

--- Array/ArraysCC.java
+++ Array/ArraysCC.java
@@ -1,11 +1,19 @@
-package Array;
+import java.util.Scanner;
 
-import java.util.*;
+public class ArraysCC{
+    public static void main(String[] args){
+        int marks[] = new int[100];
 
-public class ArraysCC {
-    public static void main(String[] args) {
-        int marks[]  = new int[50];
-        
+        Scanner sc = new Scanner(System.in);
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println("phy: " + marks[0]);
+        System.out.println("chem: " + marks[1]);
+        System.out.println("math: " + marks[2]);
+
+        marks[2] = 100;
+        System.out.println("math: " + marks[2]);        
     }
-    
 }
